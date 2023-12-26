47a03d28e779b9993936b8fd295b96d94151785c
leylagahramanova <leylagahramanova13@gmail.com>
2023-12-26 18:46:15 +0400

    r

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Link,
+  HashRouter,
+} from "react-router-dom";
+import React from "react";
 import './App.css';
+import Teymur from './pages/Teymur';
+import Mahir from './pages/Mahir';
+import Naza from './pages/Naza';
+import Mama from './pages/Mama';
+import Papa from './pages/Papa';
+import Card from './pages/Card';
 
-function App() {
+function App() { 
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+          <Route exact path='/' element={<Card/>}/>
+        <Route path='/mama' element={<Mama/>}/>
+        <Route path="/papa" element={<Papa />} />
+        <Route path="/naza" element={<Naza />} />
+        <Route path="/mahir" element={<Mahir />} />
+        <Route path="/teymur" element={<Teymur />} />
     </div>
+  
   );
 }
 
